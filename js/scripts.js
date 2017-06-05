18b499fb438f3fb2c199367eedcaa4a8f1416563
Alena Golovina and Anna Timofeeva <alena@>
2017-06-05 11:03:11 -0700

    create initial files, add content to html, js code for form output values array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+
 $(document).ready(function() {
-  
+ $("#submit_button").click(function(event) {
+  event.preventDefault();
+  var male1 = $("input#actor1").val();
+  var male2 = $("input#actor2").val();
+  var male3 = $("input#actor3").val();
+  var male4 = $("input#actor4").val();
+  var male5 = $("input#actor4").val();
+/*$("div#output").text(male1 + ", " + male2 + ", " + male3 + ", " + male4 + ", " + male5);*/
+
+var maleInput = [male1, male2, male3, male4, male5];
+$("div#output").text(maleInput);
+  /*var female1 = $("input#actor1").val();
+  $("div#output").text(female1);
+  var female2 = $("input#actor1").val();
+  $("div#output").text(female2);
+  var female3 = $("input#actor1").val();
+  $("div#output").text(female3);
+  var female4 = $("input#actor1").val();
+  $("div#output").text(female4);
+  var female5 = $("input#actor1").val();
+  $("div#output").text(female5);
+  */
+  });
 });
